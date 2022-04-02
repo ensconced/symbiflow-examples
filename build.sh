4391e3bdd90662759e26c61cd17ffc3e2fe501c4
Joe Barnett <joe.barnett@cambridge-intelligence.com>
2022-04-02 20:59:06 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#/usr/bin/env bash
+
+set -eu
+
+export INSTALL_DIR=~/opt/symbiflow
+export FPGA_FAM="xc7"
+export TARGET="basys3" 
+export PATH="$INSTALL_DIR/$FPGA_FAM/install/bin:$PATH";
+. "$INSTALL_DIR/$FPGA_FAM/conda/etc/profile.d/conda.sh"
+
+conda activate xc7
+cd xc7
+make -C counter_test
+make download -C counter_test
+
